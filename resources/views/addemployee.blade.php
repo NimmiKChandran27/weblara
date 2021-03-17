1fcc93cf6fa0550c0c6a827836f626ec1b9f4f47
NimmiKChandran27 <mca536@rajagiri.edu>
2021-03-17 14:41:49 +0530

    first commit

---
+++ resources/views/addemployee.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- As a link -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/add">ADD EMPLOYEE</a>
+  </div>
+</nav>
+
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/search">SARCH EMPLOYEE</a>
+  </div>
+</nav>
+
+<div class="container">
+<div class="row">
+<div class="col">
+<table class="table table-borderless">
+<tr>
+  <td>EMPLOYEE NAME</td>
+  <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+  <td>EMP CODE</td>
+  <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+  <td>DESIGNATION</td>
+  <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+  <td>SALARY</td>
+  <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+  <td>DOB</td>
+  <td><input type="date" class="form-control"></td>
+</tr>
+<tr>
+  <td>PLACE</td>
+  <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+  <td>PIN CODE</td>
+  <td><input type="password" class="form-control"></td>
+</tr>
+<tr>
+  <td>PHONE</td>
+  <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td></td>
+    <td><button class="btn btn-primary">SUBMIT</button></button></td>
+</tr>
+</table>
+</div>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
